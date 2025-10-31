abea5eeab04e318657ea8233c96b0d193c1b92d9
likerhythm <wjdalsdl0412@gmail.com>
2025-10-31 16:07:10 +0900

    refactor(bonus-number): 검증 메서드의 접근 제한자 private으로 설정

--- src/main/java/lotto/base_numbers/BonusNumber.java
+++ src/main/java/lotto/base_numbers/BonusNumber.java
@@ -10,7 +10,7 @@ public class BonusNumber extends BaseNumbers {
         validate(numbers);
     }
 
-    protected void validate(List<LottoNumber> numbers) {
+    private void validate(List<LottoNumber> numbers) {
         if (numbers.size() != 1) {
             throw new IllegalArgumentException("[ERROR] 보너스 번호는 하나만 존재할 수 있습니다");
         }
