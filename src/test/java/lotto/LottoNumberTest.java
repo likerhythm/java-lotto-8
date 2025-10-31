4b7bf95bc9222526c515ae6a3cac6e14913b76f4
likerhythm <wjdalsdl0412@gmail.com>
2025-10-31 15:22:21 +0900

    test: 로또 번호 테스트 구현

---
+++ src/test/java/lotto/LottoNumberTest.java
@@ -0,0 +1,17 @@
+package lotto;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class LottoNumberTest {
+
+    @Test
+    void 로또_번호_범위보다_크면_예외가_발생합니다() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> LottoNumber.of(46));
+    }
+
+    @Test
+    void 로또_번호_범위보다_작으면_예외가_발생합니다() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> LottoNumber.of(0));
+    }
+}
