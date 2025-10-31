1de5bf805bb7462429a9862db61d84a8f2cbb0a0
likerhythm <wjdalsdl0412@gmail.com>
2025-10-31 15:27:01 +0900

    refactor(lotto): 로또 번호가 UserNumbers에 의해 관리되도록 수정

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,12 @@
 package lotto;
 
-import java.util.List;
+import lotto.base_numbers.UserNumbers;
 
 public class Lotto {
-    private final List<Integer> numbers;
 
-    public Lotto(List<Integer> numbers) {
-        validate(numbers);
+    private final UserNumbers numbers;
+
+    public Lotto(UserNumbers numbers) {
         this.numbers = numbers;
     }
-
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
-        }
-    }
-
-    // TODO: 추가 기능 구현
 }
