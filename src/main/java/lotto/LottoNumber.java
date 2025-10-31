ef5519bdcf98fbfe4f052cc68119af43e454f4b8
likerhythm <wjdalsdl0412@gmail.com>
2025-10-31 15:19:18 +0900

    feat(lotto-number): 로또 번호 관리 클래스 구현

---
+++ src/main/java/lotto/LottoNumber.java
@@ -0,0 +1,46 @@
+package lotto;
+
+public class LottoNumber implements Comparable<LottoNumber> {
+
+    private static final int MIN = 1;
+    private static final int MAX = 45;
+
+    private final int value;
+
+    private LottoNumber(int value) {
+        this.value = value;
+        validate(value);
+    }
+
+    public static LottoNumber of(int value) {
+        return new LottoNumber(value);
+    }
+
+    @Override
+    public int compareTo(LottoNumber other) {
+        return Integer.compare(this.value, other.value);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (!(other instanceof LottoNumber)) {
+            return false;
+        }
+        return this.value == ((LottoNumber) other).value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Integer.hashCode(value);
+    }
+
+    private void validate(int number) {
+        if (!inRange(number)) {
+            throw new IllegalArgumentException("[ERROR] 번호는 " + MIN + "보다 작을 수 없고 " + MAX + "보다 클 수 없습니다");
+        }
+    }
+
+    private static boolean inRange(int number) {
+        return MIN <= number && number <= MAX;
+    }
+}
