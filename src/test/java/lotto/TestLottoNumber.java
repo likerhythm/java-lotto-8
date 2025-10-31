a755d8e261b62d7d8889b890e8cf7f89b6245ed5
likerhythm <wjdalsdl0412@gmail.com>
2025-10-31 15:20:16 +0900

    test: 테스트 편의를 위해 테스트용 로또 번호 마련

---
+++ src/test/java/lotto/TestLottoNumber.java
@@ -0,0 +1,12 @@
+package lotto;
+
+public class TestLottoNumber {
+
+    public static final LottoNumber ONE = LottoNumber.of(1);
+    public static final LottoNumber TWO = LottoNumber.of(2);
+    public static final LottoNumber THREE = LottoNumber.of(3);
+    public static final LottoNumber FOUR = LottoNumber.of(4);
+    public static final LottoNumber FIVE = LottoNumber.of(5);
+    public static final LottoNumber SIX = LottoNumber.of(6);
+    public static final LottoNumber SEVEN = LottoNumber.of(7);
+}
---
+++ src/test/java/lotto/TestLottoNumbers.java
@@ -0,0 +1,14 @@
+package lotto;
+
+import static lotto.TestLottoNumber.*;
+
+import java.util.List;
+
+public class TestLottoNumbers {
+
+    public static final List<LottoNumber> BASIC_NUMBERS = List.of(ONE, TWO, THREE, FOUR, FIVE, SIX);
+    public static final List<LottoNumber> MANY_NUMBERS = List.of(ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN);
+    public static final List<LottoNumber> FEW_NUMBERS = List.of(ONE, TWO, THREE, FOUR, FIVE);
+    public static final List<LottoNumber> DUPLICATE_NUMBERS = List.of(ONE, TWO, THREE, FOUR, FIVE, FIVE);
+    public static final List<LottoNumber> UNSORTED_NUMBERS = List.of(SIX, FIVE, FOUR, THREE, TWO, ONE);
+}
