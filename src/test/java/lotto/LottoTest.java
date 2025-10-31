8f2fbf75f6bc28b4d34d101ad64a5093802e422d
likerhythm <wjdalsdl0412@gmail.com>
2025-10-31 15:28:45 +0900

    test: 로또 테스트가 UserNumbers를 사용하여 진행되도록 수정

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,5 +1,9 @@
 package lotto;
 
+import static lotto.TestLottoNumbers.*;
+
+import java.util.ArrayList;
+import lotto.base_numbers.UserNumbers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,16 +14,16 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class LottoTest {
     @Test
     void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
+        List<LottoNumber> numbers = new ArrayList<>(MANY_NUMBERS);
+        assertThatThrownBy(() -> new Lotto(new UserNumbers(numbers)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+        List<LottoNumber> numbers = new ArrayList<>(DUPLICATE_NUMBERS);
+        assertThatThrownBy(() -> new Lotto(new UserNumbers(numbers)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
-
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
 }
