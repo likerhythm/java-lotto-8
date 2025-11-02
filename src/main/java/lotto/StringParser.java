ce542a2e02611a5784866015bf28310bc44ca3e7
likerhythm <wjdalsdl0412@gmail.com>
2025-11-02 12:54:02 +0900

    feat(string-parser): 문자열 유틸 기능 구현

---
+++ src/main/java/lotto/StringParser.java
@@ -0,0 +1,23 @@
+package lotto;
+
+public class StringParser {
+
+    public static int toInteger(String input, String domain) {
+        if (input == null) {
+            throw new IllegalArgumentException("[ERROR] 알 수 없는 오류가 발생했습니다");
+        }
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] " + domain + "은/는 정수로 입력해주세요");
+        }
+    }
+
+    public static String numberFormat(int number) {
+        return String.format("%,d", number);
+    }
+
+    public static String numberFormat(double number) { // todo 둘째자리 반올림 안될걸
+        return String.format("%,1f", number);
+    }
+}
