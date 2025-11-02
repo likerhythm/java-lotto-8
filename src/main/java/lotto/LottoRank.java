69239340463bb451f426f3635798237ac59c3452
likerhythm <wjdalsdl0412@gmail.com>
2025-11-02 12:52:04 +0900

    feat(lotto-rank): 로또 등수 정보를 저장하는 Enum 구현

---
+++ src/main/java/lotto/LottoRank.java
@@ -0,0 +1,72 @@
+package lotto;
+
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public enum LottoRank {
+
+    _1ST(2_000_000_000, 6, null),
+    _2ND(30_000_000, 5, Boolean.TRUE),
+    _3RD(1_500_000, 5, Boolean.FALSE),
+    _4TH(50_000, 4, null),
+    _5TH(5_000, 3, null),
+    _6TH(0, 2, null),
+    _7TH(0, 1, null),
+    _8TH(0, 0, null),
+    ;
+
+    private int reward;
+    private Integer matchCount;
+    private Boolean bonusMatch;
+
+    LottoRank(int reward, Integer matchCount, Boolean bonusMatch) {
+        this.reward = reward;
+        this.matchCount = matchCount;
+        this.bonusMatch = bonusMatch;
+    }
+
+    public static LottoRank by (int matchCount, boolean bonusMatch) {
+        return Arrays.stream(LottoRank.values())
+                .filter(lr -> lr.check(matchCount, bonusMatch))
+                .findFirst()
+                .orElse(_8TH);
+    }
+
+    public int getReward() {
+        return this.reward;
+    }
+
+    public boolean isWinner() {
+        return this.reward > 0;
+    }
+
+    public static Map<LottoRank, Integer> makeWinnerLayout() {
+        Map<LottoRank, Integer> ranks = new LinkedHashMap<>();
+        for (LottoRank rank : LottoRank.values()) {
+            if (rank.isWinner()) {
+                ranks.put(rank, 0);
+            }
+        }
+        return ranks;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(this.matchCount).append("개 일치");
+        if (this.bonusMatch != null && this.bonusMatch) {
+            sb.append(", 보너스 볼 일치");
+        }
+        sb.append(" (").append(StringParser.numberFormat(this.reward)).append("원)");
+
+        return sb.toString();
+    }
+
+    private boolean check(int matchCount, boolean bonusMatch) {
+        if (this.bonusMatch != null) {
+            return this.matchCount == matchCount && this.bonusMatch == bonusMatch;
+        }
+        return this.matchCount == matchCount;
+    }
+}
