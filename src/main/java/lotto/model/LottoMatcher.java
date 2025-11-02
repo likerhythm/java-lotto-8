98ff70131e2c7a5346e18495890bc89340f86448
likerhythm <wjdalsdl0412@gmail.com>
2025-11-02 16:24:28 +0900

    refactor: 기존 LottoVerifyService를 LottoMatcher로 대체
    이유: 상태값을 가지고 있기 때문에 서비스 계층이 아닌 도메인 계층이라고 판단

--- src/main/java/lotto/model/LottoMatcher.java
+++ src/main/java/lotto/model/LottoMatcher.java
@@ -1,4 +1,4 @@
-package lotto.service;
+package lotto.model;
 
 import java.util.List;
 import java.util.Map;
@@ -8,12 +8,12 @@ import lotto.model.numbers.DrawNumbers;
 import lotto.model.lotto.Lotto;
 import lotto.model.lotto.LottoUtil;
 
-public class LottoVerifyService {
+public class LottoMatcher {
 
-    private DrawNumbers drawNumbers;
-    private List<Lotto> lottos;
+    private final DrawNumbers drawNumbers;
+    private final List<Lotto> lottos;
 
-    public LottoVerifyService(DrawNumbers drawNumbers, List<Lotto> lottos) {
+    public LottoMatcher(DrawNumbers drawNumbers, List<Lotto> lottos) {
         this.drawNumbers = drawNumbers;
         this.lottos = lottos;
     }
@@ -24,7 +24,7 @@ public class LottoVerifyService {
         return new LottoResult(ranks, rateOfReturn);
     }
 
-    private double calculateRateOfReturn(Map<LottoRank, Integer> ranks) { // todo 수익률 계산 잘 안됨
+    private double calculateRateOfReturn(Map<LottoRank, Integer> ranks) {
         long totalReward = 0;
         for (Map.Entry<LottoRank, Integer> entry : ranks.entrySet()) {
             long reward = entry.getKey().getReward();
